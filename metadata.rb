d7e4e1d74e5adbc738341256700df272e51c7211
Elijah Wright <elijah.wright@gmail.com>
2016-04-10 13:09:44 -0500

    Update.

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email  "contact@akretion.com"
 license           "Apache 2.0"
 description       "Provisions server environment for OpenERP"
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version           "0.0.1"
+version           "0.0.2"
 recipe            "ak-odoo-chef-base::default", "Default"
 depends           "python"
 depends           "apt"
